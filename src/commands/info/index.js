7156bf0d4233cd542bf3f1794d7190a6418ef549
NetherDragonTw <Twor1060595152@hotmail.com>
2024-02-05 12:18:37 +0800

    ping test

--- src/commands/info/index.js
+++ src/commands/info/index.js
@@ -1,21 +1,21 @@
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js'
+import { SlashCommandBuilder, EmbedBuilder, WebSocketManager, Client } from 'discord.js'
 
 export const command = new SlashCommandBuilder()
     .setName("info")
     .setDescription("顯示機器人資訊")
 
 export const action = async (ctx) => {
-	await ctx.channel.send({ embeds: [infoembed] })
+	await ctx.channel.send({ embeds: [infoembed], ephemeral: true });
 }
+const ping = Client.ping
 const infoembed = new EmbedBuilder()
 	.setColor(0x0099FF)
 	.setTitle('機器人資訊')
 	.setAuthor({ name: '我不是YEE龍' })
 	.setDescription('以下是目前的資訊')
 	.addFields(
-		{ name: '機器人延遲', value: 'Some value here', inline: true },
+		{ name: '機器人延遲', value: (`${ping}`) + "ms", inline: true },
 				)
-	.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.addFields({ name: '版本', value: 'Dev1.0', inline: true })
 	.setTimestamp()
-	.setFooter({ text: 'Some footer text here'});
+	.setFooter({ text: '開發中'});
